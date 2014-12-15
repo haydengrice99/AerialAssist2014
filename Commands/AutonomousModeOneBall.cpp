af9e80deda4792fd727ec81f5d54a5419d7999a7
haydengrice99 <fruitninja05@hotmail.com>
2014-12-15 15:58:32 -0500

    Autonomous class change
    Makes the robot go forward, turn, wait for 2 seconds, go forward, turn again, and wait for 2 seconds.

--- Commands/AutonomousModeOneBall.cpp
+++ Commands/AutonomousModeOneBall.cpp
@@ -14,14 +14,16 @@
 
 AutonomousModeOneBall::AutonomousModeOneBall()
 {
-  AddSequential(new AutonomousLowGearCommand());
-  AddSequential(new LoadCatapultGroup());
-  AddSequential(new OperatorLowCommand());
-  AddSequential(new AutonomousPickupCommand(2.0));
-  AddSequential(new OperatorPickup2Command());
-  AddSequential(new OperatorHighCommand());
   AddSequential(new AutonomousDriveCommand(1.0f, TURN_CORRECTION, 2.2));
-  AddParallel(new OperatorPickup2Command(0.5));
-  AddSequential(new AutonomousCoastCommand(1.0f, TURN_CORRECTION, 0.0f, TURN_CORRECTION, 0.5));
-  AddSequential(new DriveLaunchReleaseCommand());
+  AddSequential(new AutonomousDriveCommand(0, TURN_CORRECTION, 0));
+  Wait(2.0f);
+  AddSequential(new AutonomousDriveCommand(1.0f, TURN_CORRECTION, 2.2));
+  AddSequential(new AutonomousDriveCommand(1.0f, TURN_CORRECTION, 2.2));
+  Wait(2.0f); 
+  AddSequential(new AutonomousDriveCommand(1.0f,-1.0f, 0.2));
+  AddSequential(new AutonomousDriveCommand(0.0f, TURN_CORRECTION, 0.0f));
+  Wait(2.0f)
+  AddSequential(new AutonomousDriveCommand(1.0f, TURN_CORRECTION, 2.2));
+  AddSequential(new AutonomousDriveCommand(0.0f, TURN_CORRECTION, 0));
+  Wait(2.0f
 }
